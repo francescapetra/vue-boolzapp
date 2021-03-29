309bee9f7222a7052711e1d287152fe6fe5a4c18
francescapetra <francescalaurapetra@gmail.com>
2021-03-29 11:13:23 +0200

    background-gradient

--- script/script.js
+++ script/script.js
@@ -1,9 +1,10 @@
+// Milestone 1
+// ●Replica della graficacon la possibilità di averemessaggi scritti dall’utente (verdi) edall’interlocutore (bianco) assegnando due classiCSS diverse●Visualizzazione dinamica della lista contatti:tramitela direttiva v-for, visualizzarenome e immagine di ogni contatto
 var app = new Vue(
   {
     el:"#root",
     data:{
-      message: "",
-      miaClasse : "red",
+
     },
     methods:{
 
