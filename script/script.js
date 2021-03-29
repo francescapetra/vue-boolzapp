87b71f997e8e4135f8ff1ca5bcedba631f7a5643
francescapetra <francescalaurapetra@gmail.com>
2021-03-29 16:59:31 +0200

    overflow

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // Milestone 1
-// ●Replica della graficacon la possibilità di averemessaggi scritti dall’utente (verdi) edall’interlocutore (bianco) assegnando due classiCSS diverse●Visualizzazione dinamica della lista contatti:tramitela direttiva v-for, visualizzarenome e immagine di ogni contatto
+// ●Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) edall’interlocutore (bianco) assegnando due classiCSS diverse●Visualizzazione dinamica della lista contatti:tramitela direttiva v-for, visualizzarenome e immagine di ogni contatto
 var app = new Vue(
   {
     el:"#root",
